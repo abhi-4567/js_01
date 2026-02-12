c129b5db59e800b8325e44da3903a46a09795dc7
abhi-4567 <thesharmaabhinav4567@gmail.com>
2026-02-12 18:25:20 +0000

    day 02

---
+++ javascript/strings.js
@@ -0,0 +1,34 @@
+const name = "abhinav"
+const age = 20
+
+// console.log("my name is "+name+" and i am "+age+" years old")
+// console.log(`my name is ${name} and i am ${age}years old`)
+
+const  schoolName = new String("T.T.S Academy")
+
+// console.log(schoolName);          //do not use . to access elements
+
+// console.log(typeof schoolName)
+
+// console.log(schoolName.__proto__); __proto__ not used  instead Object.getPrototypeOf(string_name)
+
+// console.log(schoolName.charAt(-1))  it does not accept - index but "at" does and same for slice and substring
+
+// console.log(schoolName.concat(" abhinav ", "sharma"));
+
+// console.log(schoolName); does not change upon concatenation
+
+// at(),charAt(),concat(str1,str2),constructor,includes(),indexOf(),length,padEnd(length,fillString), padStart,replace(  ,  ),replaceAll(  ,  ),slice(  ,  ),split(),substring(  , ),tolowerCase() with tolocaleLowerCase() and upperCase also, toString(), toLocaleString(),trim(),hasOwnProperty(),isPrototypeOf(),propertyIsEnumerable()
+
+// console.log(schoolName.replace("T", "r")); replace  only changes first occurrence
+
+
+
+
+
+
+
+
+
+
+
