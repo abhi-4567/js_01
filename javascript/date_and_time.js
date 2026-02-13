b890163e0a30e32dae8a2c4edbb6327bfbc12b13
abhi-4567 <thesharmaabhinav4567@gmail.com>
2026-02-13 14:55:54 +0000

    day 3

---
+++ javascript/date_and_time.js
@@ -0,0 +1,23 @@
+// const myDate =new Date()
+// console.log(myDate);
+// let myDate =new Date(13,1,2026,20,10) //cant write it like thiss  ....also no octals
+let myDate = new Date(2026,1,13,20,15)
+// console.log(myDate);
+
+// console.log(myDate.getFullYear()); //day month year(full)
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString('default' ,{
+//     weekday: 'long'
+// }));      myDate.toLocaleString(locales, options) .....locale: "default" or "en-IN" etc ....options is an object {weekday :" ", day: " ", month: " ", etc......}
+
+
+let myDate1= new Date("2-13-2026") //here datestring has month from 1 unlike 0 and 2 formatss yyyy-mm-dd  mm-dd-yyyy
+console.log(myDate1);
+
+
+
+
+
+
+
---
+++ javascript/nums_and_maths.js
@@ -0,0 +1,14 @@
+const nums1 = 200
+// console.log(nums1);
+// console.log(nums1.toString().length)
+// console.log(nums1.toFixed(2))
+
+// const nums2= new Number(1000000)
+// // console.log(nums2.toPrecision(4))
+// console.log(nums2.toLocaleString("en-US"))
+
+// console.log(Math)
+// console.log(Math.abs(-4.56)) round, ceil, floor, min, max
+
+// console.log(Math.floor((Math.random())*20)+1) this also covers min and max case of range*/
+
