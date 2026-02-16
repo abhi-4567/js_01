bfc53756d9fef94aa537dcce18df3f980717c05b
abhi-4567 <thesharmaabhinav4567@gmail.com>
2026-02-16 06:09:46 +0000

    day6

---
+++ iterations/seven.js
@@ -0,0 +1,11 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+                                       //.....map and filter can return unlike forEach
+
+// const newNums = myNumers
+//                 .map((num) => num * 10 ) 
+//                 .map( (num) => num + 1)       ......chaining
+//                 .filter( (num) => num >= 40)
+
+console.log(newNums);
