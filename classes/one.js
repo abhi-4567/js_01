1b8eab6406f163eaa614bec01956bb120796b185
abhi-4567 <thesharmaabhinav4567@gmail.com>
2026-02-21 09:28:35 +0000

    day 9

---
+++ classes/one.js
@@ -0,0 +1,35 @@
+//object literral
+
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        console.log("Got user details from database");
+        console.log(`Username: ${this.username}`); //tells ref is cur obj
+        console.log(this);
+    }
+
+}
+// user.getUserDetails()
+// console.log(this)
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+    this.greeting()
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(userOne);
